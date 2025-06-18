c3215577d56091daff4f53ad20a2b4b20044593a
myselfsandip <sandipsingha714@gmail.com>
2025-06-19 01:06:28 +0530

    Created Auth Layout

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+function Page() {
+    console.log('Sign In Page');
+    return (
+        <SignInView />
+    );
+}
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,78 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client"; //import the auth client
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Loader } from "lucide-react";
+
 
 export default function Home() {
-  return <div >
-    <Button variant="custom">
-      Click Me
-    </Button>
+  const { data: session, isPending } = authClient.useSession();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password
+    }, {
+      onSuccess: (ctx) => {
+        alert('Success');
+      },
+      onError: (ctx) => {
+        const errorMessage = ctx.error?.message || JSON.stringify(ctx.error);
+        alert(errorMessage);
+      }
+    }
+    )
+  }
+
+  const onLogin = () => {
+    authClient.signIn.email({
+      email,
+      password
+    }, {
+      onSuccess: (ctx) => {
+        alert('Login Successfull');
+      },
+      onError: (ctx) => {
+        const errorMessage = ctx.error?.message || JSON.stringify(ctx.error);
+        alert(errorMessage);
+      }
+    }
+    )
+  }
+
+  if (isPending) {
+    return <div className="flex justify-center items-center h-screen">
+      <Loader className="animate-spin w-16 h-16 text-red-500" />
+    </div>
+  }
+
+  if (session) {
+    return <div className="flex flex-col p-4 gap-y-4">
+      <p>Logged in as  {session.user.name}</p>
+
+      <Button onClick={() => authClient.signOut()}>
+        Sign out
+      </Button>
+    </div>
+  }
+
+  return <div className="flex flex-col gap-y-10">
+    <div className="p-4 flex flex-col gap-y-4">
+      <Input placeholder="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <Input placeholder="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={onSubmit}>Sign up</Button>
+    </div>
+    <div className="p-4 flex flex-col gap-y-4">
+      <Input placeholder="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <Button onClick={onLogin}>Login</Button>
+    </div>
   </div>
 }
