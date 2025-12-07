d9966303abb1eeba45d795dacaaf0c8063bc6666
myselfsandip <sandipsingha714@gmail.com>
2025-12-08 01:18:52 +0530

    added tsup for ts build

---
+++ backend/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ['src/index.ts'], // Your entry point
+    format: ['esm'],         // Output as ESM (mjs/js)
+    dts: false,              // Don't generate type files for prod
+    splitting: false,        // Keep it simple
+    sourcemap: true,         // Good for debugging production errors
+    clean: true,             // Clean dist folder before build
+    target: 'node20',        // Target your Node version
+});
